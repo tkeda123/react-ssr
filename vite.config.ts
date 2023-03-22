e270d0435ec9f65d97a625691d64488813853fef
tatsuya <takeda.kiyosi@icloud.com>
2023-03-22 16:47:44 +0900

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-    ? 'react-ssr' // レポジトリ名を設定
-    : './',
+  base: process.env.GITHUB_PAGES ? 'react-ssr' : './',
   plugins: [react()],
 });
