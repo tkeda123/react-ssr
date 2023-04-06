6aa5c5909af52e71380e345dd3134f33c8d4926a
tatsuya <takeda.kiyosi@icloud.com>
2023-04-06 23:11:01 +0900

    add

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+hydrateRoot(document.getElementById('root'), _jsx(React.StrictMode, { children: _jsx(App, {}) }));
